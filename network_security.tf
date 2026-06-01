0f88d88e18749d3ce08b106283be5c192c587868
Shreya Shedge <shreyashedge675@gmail.com>
2026-06-01 19:55:39 +0530

    final

--- network_security.tf
+++ network_security.tf
@@ -20,10 +20,19 @@ resource "aws_security_group" "ssh_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH outbound"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ip_range
+  }
+
+  egress {
+    description = "ICMP outbound"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = var.allowed_ip_range
   }
 
   tags = {
@@ -53,10 +62,19 @@ resource "aws_security_group" "public_http_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP outbound"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ip_range
+  }
+
+  egress {
+    description = "ICMP outbound"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = var.allowed_ip_range
   }
 
   tags = {
